a675bfab56cf0084a832e602723e1b8249e05a6e
Nicholas Gunner <ngunner@orbitist.com>
2018-07-14 09:04:35 -0600

    setup featured properties bar

--- js/drawer.js
+++ js/drawer.js
@@ -1,4 +1,4 @@
-function flyToProject(lng, lat) {
+function flyToProperty(lng, lat) {
   map.flyTo({
       center: [lng, lat],
       zoom: 18,
@@ -10,11 +10,11 @@ function flyToProject(lng, lat) {
   facingNorth = false;
 };
 
-for (var i = 0; i < projectsFeed.features.length; i++) {
-  if (projectsFeed.features[i].properties.featured == "True" && projectsFeed.features[i].properties.tile.length > 5) {
-    $(".drawer-content").append('<div onclick="flyToProject(\'' + projectsFeed.features[i].geometry.coordinates[0] + '\', \'' + projectsFeed.features[i].geometry.coordinates[1] + '\')" class="o-tile"><div class="o-tile-img Post"><img src="https://westfieldny.com' + projectsFeed.features[i].properties.tile + '" width="200" height="200" typeof="Image" class="img-responsive"><a class="o-tile-icon Post" aria-label="Icon linking to a marker" alt="An icon" href="#"></a></div><div class="o-tile-content"><div class="o-tile-content-title">' + projectsFeed.features[i].properties.name + '</div></div></div>');
-  } else if (projectsFeed.features[i].properties.featured == "True" && projectsFeed.features[i].properties.tile.length < 5) {
-      $(".drawer-content").append('<div onclick="flyToProject(\'' + projectsFeed.features[i].geometry.coordinates[0] + '\', \'' + projectsFeed.features[i].geometry.coordinates[1] + '\')" class="o-tile"><div class="o-tile-img Post"><a class="o-tile-icon Post" aria-label="Icon linking to a marker" alt="An icon" href="#"></a></div><div class="o-tile-content"><div class="o-tile-content-title">' + projectsFeed.features[i].properties.name + '</div></div></div>');
+for (var i = 0; i < propertiesFeed.features.length; i++) {
+  if (propertiesFeed.features[i].properties.featured == "True" && propertiesFeed.features[i].properties.tile.length > 5) {
+    $(".drawer-content").append('<div onclick="flyToProperty(\'' + propertiesFeed.features[i].geometry.coordinates[0] + '\', \'' + propertiesFeed.features[i].geometry.coordinates[1] + '\')" class="o-tile"><div class="o-tile-img Post"><img src="https://chqlandbank.org' + propertiesFeed.features[i].properties.tile + '" width="200" height="200" typeof="Image" class="img-responsive"><a class="o-tile-icon Post" aria-label="Icon linking to a marker" alt="An icon" href="#"></a></div><div class="o-tile-content"><div class="o-tile-content-title">' + propertiesFeed.features[i].properties.name + '</div></div></div>');
+  } else if (propertiesFeed.features[i].properties.featured == "True" && propertiesFeed.features[i].properties.tile.length < 5) {
+      $(".drawer-content").append('<div onclick="flyToProperty(\'' + propertiesFeed.features[i].geometry.coordinates[0] + '\', \'' + propertiesFeed.features[i].geometry.coordinates[1] + '\')" class="o-tile"><div class="o-tile-img Post"><a class="o-tile-icon Post" aria-label="Icon linking to a marker" alt="An icon" href="#"></a></div><div class="o-tile-content"><div class="o-tile-content-title">' + propertiesFeed.features[i].properties.name + '</div></div></div>');
   }
 }
 
