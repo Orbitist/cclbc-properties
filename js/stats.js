9be5d0510b923b3eb4f349f43806fe5d6b53ae07
Nicholas Gunner <ngunner@orbitist.com>
2018-07-14 08:51:30 -0600

    added statistics

--- js/stats.js
+++ js/stats.js
@@ -1,23 +1,23 @@
-var projectsAPI = 'https://westfieldny.com/api/geojson/projects';
+var propertiesAPI = 'https://chqlandbank.org/api/geojson/properties';
 
-var projectsFeed = (function () {
-    var projectsData = null;
+var propertiesFeed = (function () {
+    var propertiesData = null;
     $.ajax({
         'async': false,
         'global': false,
-        'url': projectsAPI,
+        'url': propertiesAPI,
         'dataType': "json",
         'success': function (data) {
-            projectsData = data;
+            propertiesData = data;
         }
     });
-    return projectsData;
+    return propertiesData;
 })();
 
-var cost;
-var privateInvestment;
-var localInvestment;
-var grants;
+var propertiesMitigated;
+var rehabilitations;
+var sidelots;
+var demolitions;
 
 // Add commas to numbers
 $.fn.digits = function(){
@@ -27,71 +27,27 @@ $.fn.digits = function(){
 }
 
 function runStats() {
-  cost = 0;
-  privateInvestment = 0;
-  localInvestment = 0;
-  grants = 0;
+  propertiesMitigated = 0;
+  rehabilitations = 0;
+  sidelots = 0;
+  demolitions = 0;
 
-  if (formProjectStatus == 'Any Project Status' && formProjectType == 'Priority Projects') {
-    for (var i = 0; i < projectsFeed.features.length; i++) {
-      if (projectsFeed.features[i].properties.featured == 'True') {
-        cost = cost + parseInt(projectsFeed.features[i].properties.cost, 10);
-        privateInvestment = privateInvestment + parseInt(projectsFeed.features[i].properties.private, 10);
-        localInvestment = localInvestment + parseInt(projectsFeed.features[i].properties.local, 10);
-        grants = grants + parseInt(projectsFeed.features[i].properties.grants, 10);
-      }
+  for (var i = 0; i < propertiesFeed.features.length; i++) {
+    propertiesMitigated = propertiesMitigated + 1;
+    if (propertiesFeed.features[i].properties.category == 'Rehab') {
+      rehabilitations = rehabilitations + 1;
+    } else if (propertiesFeed.features[i].properties.category == 'Sidelot') {
+      sidelots = sidelots + 1;
     }
-  } else if (formProjectStatus != 'Any Project Status' && formProjectType == 'Priority Projects') {
-    for (var i = 0; i < projectsFeed.features.length; i++) {
-      if (formProjectStatus == projectsFeed.features[i].properties.status && projectsFeed.features[i].properties.featured == 'True') {
-        cost = cost + parseInt(projectsFeed.features[i].properties.cost, 10);
-        privateInvestment = privateInvestment + parseInt(projectsFeed.features[i].properties.private, 10);
-        localInvestment = localInvestment + parseInt(projectsFeed.features[i].properties.local, 10);
-        grants = grants + parseInt(projectsFeed.features[i].properties.grants, 10);
-      }
-    }
-  }
-  else if (formProjectStatus == 'Any Project Status' && formProjectType == 'Any Project Type') {
-    for (var i = 0; i < projectsFeed.features.length; i++) {
-      cost = cost + parseInt(projectsFeed.features[i].properties.cost, 10);
-      privateInvestment = privateInvestment + parseInt(projectsFeed.features[i].properties.private, 10);
-      localInvestment = localInvestment + parseInt(projectsFeed.features[i].properties.local, 10);
-      grants = grants + parseInt(projectsFeed.features[i].properties.grants, 10);
-    }
-  } else if (formProjectStatus == 'Any Project Status' && formProjectType != 'Any Project Type') {
-    for (var i = 0; i < projectsFeed.features.length; i++) {
-      if (formProjectType == projectsFeed.features[i].properties.type) {
-        cost = cost + parseInt(projectsFeed.features[i].properties.cost, 10);
-        privateInvestment = privateInvestment + parseInt(projectsFeed.features[i].properties.private, 10);
-        localInvestment = localInvestment + parseInt(projectsFeed.features[i].properties.local, 10);
-        grants = grants + parseInt(projectsFeed.features[i].properties.grants, 10);
-      }
-    }
-  } else if (formProjectStatus != 'Any Project Status' && formProjectType == 'Any Project Type') {
-    for (var i = 0; i < projectsFeed.features.length; i++) {
-      if (formProjectStatus == projectsFeed.features[i].properties.status) {
-        cost = cost + parseInt(projectsFeed.features[i].properties.cost, 10);
-        privateInvestment = privateInvestment + parseInt(projectsFeed.features[i].properties.private, 10);
-        localInvestment = localInvestment + parseInt(projectsFeed.features[i].properties.local, 10);
-        grants = grants + parseInt(projectsFeed.features[i].properties.grants, 10);
-      }
-    }
-  } else if (formProjectStatus != 'Any Project Status' && formProjectType != 'Any Project Type') {
-    for (var i = 0; i < projectsFeed.features.length; i++) {
-      if (formProjectStatus == projectsFeed.features[i].properties.status  && formProjectType == projectsFeed.features[i].properties.type) {
-        cost = cost + parseInt(projectsFeed.features[i].properties.cost, 10);
-        privateInvestment = privateInvestment + parseInt(projectsFeed.features[i].properties.private, 10);
-        localInvestment = localInvestment + parseInt(projectsFeed.features[i].properties.local, 10);
-        grants = grants + parseInt(projectsFeed.features[i].properties.grants, 10);
-      }
+    else if (propertiesFeed.features[i].properties.category == 'Demolition') {
+      demolitions = demolitions + 1;
     }
   }
 
-
-  $('#cost').html(cost);
-  $('#private').html(privateInvestment);
-  $('#local').html(localInvestment);
-  $('#grants').html(grants);
+  $('#propertiesMitigated').html(propertiesMitigated);
+  $('#rehabilitations').html(rehabilitations);
+  $('#sidelots').html(sidelots);
+  $('#demolitions').html(demolitions);
 }
 
 $( document ).ready(function() {
